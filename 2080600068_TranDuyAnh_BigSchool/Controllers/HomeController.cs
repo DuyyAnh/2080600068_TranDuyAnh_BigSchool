fd80c6134b5ca1af9cff08fbfc289e1972e80e86
admin <admin@DESKTOP-5ADS8E2>
2023-03-09 20:43:47 +0700

    Add project files.

---
+++ 2080600068_TranDuyAnh_BigSchool/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using _2080600068_TranDuyAnh_BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2080600068_TranDuyAnh_BigSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var upcomingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcomingCourses);
+        }
+
+    }
+}
---
+++ 2080600068_TranDuyAnh_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2080600068_TranDuyAnh_BigSchool.Startup))]
+namespace _2080600068_TranDuyAnh_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
